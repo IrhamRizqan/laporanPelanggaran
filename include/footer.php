c1ce92019779634137bcdd5489d6a75ec32fe7b8
IrhamR <59431897+IrhamRizqan@users.noreply.github.com>
2023-01-31 13:54:20 +0700

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,13 @@
+
+		<!-- Footer -->
+		<footer class="text-center">
+			<div class="footer-below">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12">
+							Copyright &copy; AP^PS 2015 | Design by <a href='#'>Feisal Jamaludin Kahfi</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
